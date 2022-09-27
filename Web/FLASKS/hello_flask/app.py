f5f0545531880e70e54996c10a6f9f6fb457628a
Safety-code <jomens136@gmail.com>
2022-09-27 16:09:17 +0000

    Added new files

--- Web/FLASKS/hello_flask/app.py
+++ Web/FLASKS/hello_flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import datetime
 import re
+import app
 
 
 app = Flask(__name__)
@@ -13,7 +14,8 @@ def index():
 @app.route("/hello/<name>")
 def hello_there(name):
     now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    formatted_now = now.strftime("%a, %d %b, %y at %X")
+    'Wednesday, 28 August, 2019 at 15:08:00'
 
 
     #filter the name argument to letters only using regular expressions. URL arguments
