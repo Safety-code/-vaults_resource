df919ce238af6bbaa773aeead44aa4cf337e6561
Safety-code <jomens136@gmail.com>
2023-12-18 12:44:59 +0000

    Updated few files

--- Scripting/Pyscript/func.py
+++ Scripting/Pyscript/func.py
@@ -1,15 +1,171 @@
-#PETS - POSTIONAL ARGUMENTS
-def describe_pets(pet_name, animal_type='dog'):
-    """Display information about your pet"""
-    print(f"\nI have {animal_type}")
-    print(f"My {animal_type}'s name is {pet_name.title()}.")
+# #PETS - POSTIONAL ARGUMENTS
+# def describe_pets(pet_name, animal_type='dog'):
+#     """Display information about your pet"""
+#     print(f"\nI have {animal_type}")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-my_pet = describe_pets('harry', 'hamster')
-my_pet = describe_pets(pet_name='harry', animal_type='hamster') # default value argument
-my_pet = describe_pets(animal_type='hamster', pet_name='harry') # keyword argument
-my_pet = describe_pets('willie') # default value argument
-my_pet = describe_pets(pet_name='willie') # default value argument
-my_pet = describe_pets()
+# my_pet = describe_pets('harry', 'hamster')
+# my_pet = describe_pets(pet_name='harry', animal_type='hamster') # default value argument
+# my_pet = describe_pets(animal_type='hamster', pet_name='harry') # keyword argument
+# my_pet = describe_pets('willie') # default value argument
+# my_pet = describe_pets(pet_name='willie') # default value argument
+# #my_pet = describe_pets()
 
-print(my_pet)
+#print(my_pet)
 
+#RETURN VALUES
+#RETURNING A DICTIONARY TO A FUNCTION
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly foramatted"""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix', 'bee')
+musician = get_formatted_name('joseph', 'mensah')
+print(musician)
+
+
+
+def build_person(first_name, last_name,age=None, occupation=None):
+    """Return a dictionary of information about a person"""
+    person = {'first':first_name, 'last':last_name}
+    if age:
+        person['age'] = age
+    if occupation:
+        person['occupation'] = occupation
+    return person
+
+musician = build_person('jimi', 'hendrix', 25, 'programmer')
+print(musician)
+
+
+# USING DICTIONARIES WITH WHILE LOOPS
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly foramatted"""
+    full_name = f"{first_name}  {last_name}"
+    return full_name.title()
+
+#This is an infinite loop
+while True:
+    print("\nPlease tell me your name")
+    print("\nEnter 'q' to quit the program")
+    f_name = input("First Name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last Name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")
+
+
+
+# PASSING A LIST
+
+def greet_users(names):
+    """Pass a simple greeting to each user in the list"""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+
+usernames = ['hannah', 'joe']
+greet_users(usernames)
+
+# WITHOUT USING FUNCTIONS
+# Start with designs that needs to be printed
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+# Simulate printing each designs until none is left
+# Move each design to completed_models after printing
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"\nPrinting Model: {current_design}")
+    completed_models.append(current_design)
+
+# Display all completed Models
+print("\nThe following models have been printed")
+for completed_model in completed_models:
+    print(completed_model)
+
+
+#USING FUNCTIONS TO ORGANIZE THE ABOVE CODE
+
+# first function takes care of printing the models
+def print_models(unprinted_designs, completed_models):
+    """Simulate printing each designs until none is left
+    Move each design to completed_models after printing
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"\nPrinting Models: {current_design}")
+        completed_models.append(current_design)
+
+# second function displays the models after printing
+def show_completed_models(completed_models):
+    """Show all the models that were printed"""
+    print("\nThe following models have been printed: ")
+    for completed_model in completed_models:
+        print(completed_model)
+
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
+
+
+
+# PASSING ARBITRARY NUMBER OF ARGUMENTS
+def make_pizza(*toppings):
+    """Print list of toppings that have been provided"""
+    print(toppings)
+
+
+topping = make_pizza('pepperoni')
+topping = make_pizza('mushrooms', 'green peppers', 'extra cheese')
+print(topping)
+
+
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make"""
+    print("\nMaking a pizza with the following toppings")
+    for topping in toppings:
+        print(f"-{topping}")
+
+
+top = make_pizza('pepperoni')
+top = make_pizza('mushrooms', 'green peppers', 'extra cheese')
+print(top)
+
+# MIXING POSTIONAL AND ARBITRARY ARGUMENTS
+
+
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make"""
+    print(f"\nMaking {size}-inch pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"-{topping}")
+
+
+top = make_pizza(16, 'pepperoni')
+top = make_pizza(25,'mushrooms', 'green peppers', 'extra cheese')
+print(top)
+
+
+# USING AN ARBITRARY KEYWORD ARGUMENTS
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user"""
+    user_info['first name'] = first
+    user_info['last name'] = last
+    return user_info
+
+user_profile = build_profile('albert', 'einstein', location='Princeton', field='physics')
+print(user_profile)
--- Scripting/Pyscript/try_it.py
+++ Scripting/Pyscript/try_it.py
@@ -99,7 +99,153 @@ def describe_city(city_name, country='ghana'):
     """Describe the city"""
     print(f"{city_name.title()} is in {country.title()}")
 
-city  = describe_city('acra')
+city  = describe_city('accra')
 city = describe_city(city_name='takoradi')
 city = describe_city(city_name='abidjan', country='ivory coast')
 print(city)
+
+# 8-6 CITY NAMES
+def city_country(city_name, country):
+    """Return a well formatted city and the corresponding country"""
+    city = f"{city_name} {country}"
+    return city.title()
+
+country_city = city_country("'santiago',", "'chile'")
+country_city = city_country("'accra',", "'ghana'")
+country_city = city_country("'manchester',", "'england'")
+print(country_city)
+
+
+# 8-7 ALBUMS
+
+def make_album(artist_name, album_title, No_songs=None):
+    """Return a dictionary of an artist and album"""
+    music_album = {'artist':artist_name, 'album title':album_title}
+    if No_songs:
+        music_album['No_songs'] = No_songs
+    return music_album
+
+
+catalog = make_album('Sarkodie', 'Mary', 5)
+print(catalog)
+catalog = make_album('Kidi', 'Golden Boy')
+print(catalog)
+
+
+# 8-8 USER ALBUMS
+
+def make_album(artist_name, album_title, No_songs=None):
+    """Return a dictionary of an artist and album"""
+    music_album = {'Artist':artist_name, 'Album_title':album_title}
+    return music_album
+
+
+#infinite loop
+while True:
+    print("\nPrint Artist Details:")
+    print("(Enter 'q' anywhere to quit)")
+    art_name = input("Enter artist name: ")
+    if art_name == 'q':
+        break
+    alb_name = input("Enter artist album name: ")
+    if alb_name == 'q':
+        break
+
+
+    formatted_catalog = make_album(art_name, alb_name)
+    print(f"{formatted_catalog}")
+
+# 8-9 MESSAGES
+def show_messages(messages):
+    """Passing a list of messages"""
+    for message in messages:
+        msg = f"{message.title()}"
+        print(msg)
+
+short_msgs = ['hello pal', 'call me', 'am busy']
+show_messages(short_msgs)
+
+# 8-10 SENDING MESSAGES
+def send_messages(unsent_msgs, sent_messages):
+    """Passing a list of text messages """
+    while unsent_msgs:
+        current_messages = unsent_msgs.pop()
+        print(f"\nPrinting unsent messages: {current_messages}")
+        sent_messages.append(current_messages)
+
+
+def show_sent_messages(sent_messages):
+    """Display sent messages"""
+    print(f"\nThe following messages have been sent: ")
+    for sent_message in sent_messages:
+        print(sent_message)
+
+
+unsent_msgs = ['hello pal', 'call me later', 'am busy']
+sent_messages = []
+
+send_messages(unsent_msgs, sent_messages)
+show_sent_messages(sent_messages)
+
+# 8--11 ARCHIVED MESSAGES
+def send_messages(unsent_msgs, sent_messages):
+    """Passing a list of text messages """
+    while unsent_msgs:
+        current_messages = unsent_msgs.pop()
+        print(f"\nPrinting unsent messages: {current_messages}")
+        sent_messages.append(current_messages)
+
+
+def show_sent_messages(sent_messages):
+    """Display sent messages"""
+    print(f"\nThe following messages have been sent: ")
+    for sent_message in sent_messages:
+        print(sent_message)
+
+
+unsent_msgs = ['hello pal', 'call me later', 'am busy']
+sent_messages = []
+
+send_messages(unsent_msgs[:], sent_messages)
+show_sent_messages(sent_messages)
+
+
+# 8-12 SANDWICHES
+
+def make_sandwich(*sandwich_items):
+    """Build sandwich items and describe them"""
+    print("\nMaking ordered sandwich items...")
+    for item in sandwich_items:
+        print(f"-{item}")
+
+sand_item = make_sandwich("pepperoni")
+sand_item = make_sandwich("cheese", "green pepper", "orange")
+sand_item = make_sandwich("mushrooms", "pepper", "fruits")
+print(sand_item)
+
+
+# 8-13 BUILD PROFILE
+
+def build_profile(first_name, last_name, **kwargs):
+    """Building profile about a person."""
+    kwargs['first_name'] = first_name
+    kwargs['last name'] = last_name
+    return kwargs
+
+user_profile = build_profile('Degrasse', 'Tyson', location='United States', fields= 'Astrophysics', Born='1975')
+print(user_profile)
+
+# 8- 14 CARS
+
+def car_info(manufacturer, model_name, **kwargs):
+    """Describe different car models"""
+    kwargs['manufacturer'] = manufacturer
+    kwargs['model name'] = model_name
+    return kwargs
+
+
+car_profile = car_info('Volkswaggen','Y-21', location='Germany', color='Aurbegine', tow_package=True)
+print(car_profile)
+
+car_profile = car_info('Buggati', 'Veron', location='USA', color='Mate Green', tow_package=True)
+print(car_profile)
